3e939a272a92e54bc30e33275257081c969ad203
ddas4548 <ddas4548@gmail.com>
2024-07-30 08:08:35 +0530

    sidebar is completed

--- src/components/Folder.jsx
+++ src/components/Folder.jsx
@@ -1,25 +1,61 @@
-import { useState } from "react"
 
+import { useState } from "react";
 
-// eslint-disable-next-line react/prop-types
-export default function Folder({explorer}) {
-   const [expand, setExpand] = useState(false)
+export default function Folder({ explorer,handleInsertNode }) {
+  const [expand, setExpand] = useState(false);
+  const [showInput, setShowInput] = useState({
+    visible: false,
+    isFolder: null,
+  });
+  function handleNewFolder(e, isFolder) {
+    e.stopPropagation();
+    setExpand(true)
+    setShowInput({
+      visible: true,
+      isFolder,
+    });
+  }
 
+  const onAddFolder =(e)=>{
+    if(e.keyCode === 13 && e.target.value){
+      handleInsertNode(explorer.id, e.target.value, showInput.isFolder)
+      setShowInput({
+        ...showInput,
+        visible:false
+      })
+    }
+  }
 
- if(explorer.isFolder){
-  return (
-    <div style={{ marginTop: 5}}>
-        <div className="folder" onClick={()=>setExpand(!expand)}>
-         <span>📁 {explorer.name} </span>
+  if (explorer.isFolder) {
+    return (
+      <div style={{ marginTop: 5 }}>
+        <div className="folder" onClick={() => setExpand(!expand)}>
+          <span>📁 {explorer.name} </span>
+          <div>
+            <button style={{ cursor: "pointer"}} onClick={(e) => handleNewFolder(e, true)}>📁+</button>
+            <button style={{ cursor: "pointer"}} onClick={(e) => handleNewFolder(e, false)}>📃+</button>
+          </div>
         </div>
-        <div style={{display: expand? 'block' : 'none', paddingLeft:25}}>
-            {explorer.items.map((exp)=>(
-               <Folder explorer={exp} key={exp.id} />
-            ))}
+        <div style={{ display: expand ? "block" : "none", paddingLeft: 25 }}>
+          {showInput.visible && (
+            <div className="inputContainer">
+              <span>{showInput.isFolder ? "📁" : "📃"}</span>
+              <input
+                className="inputContainer__input"
+                onKeyDown={onAddFolder}
+                type="text"
+                autoFocus
+                onBlur={() => setShowInput({ ...showInput, visible: false })}
+              />
+            </div>
+          )}
+          {explorer.items.map((exp) => (
+            <Folder explorer={exp} key={exp.id} handleInsertNode={handleInsertNode} />
+          ))}
         </div>
-    </div>
-  )}
-  else{
-    return <span className="file">📃{explorer.name}</span>
+      </div>
+    );
+  } else {
+    return <span className="file">📃{explorer.name}</span>;
   }
 }
--- src/components/FolderComp.jsx
+++ src/components/FolderComp.jsx
@@ -2,13 +2,20 @@
 import { useState } from "react"
 import explorer from "../data/folderData"
 import Folder from "./Folder"
+import useTraverseTree from "../hooks/useTraverseTree"
 export default function FolderComp() {
 
-    const [explorerData, setExplorerData] = useState(explorer)
+  const [explorerData, setExplorerData] = useState(explorer)
+  const {insertNode} = useTraverseTree();
+  const handleInsertNode =(folderId,item,isFolder)=>{
+    const finalTree = insertNode(explorerData,folderId,item,isFolder)
+
+    setExplorerData(finalTree)
+  }
 
   return (
     <div>
-      <Folder explorer={explorerData}/>
+      <Folder explorer={explorerData} handleInsertNode={handleInsertNode} />
     </div>
   )
 }
